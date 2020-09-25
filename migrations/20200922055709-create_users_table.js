d7176896c3fde222b8fcd985c34e52946af3c66b
Captain IR <52345388+Captain-IR@users.noreply.github.com>
2020-09-25 10:41:37 +0200

    Changed username to email

--- migrations/20200922055709-create_users_table.js
+++ migrations/20200922055709-create_users_table.js
@@ -10,7 +10,7 @@ module.exports = {
         primaryKey: true,
       },
     
-      username: {
+      email: {
         type: Sequelize.STRING,
         allowNull: false,
         unique: true,
--- models/User.js
+++ models/User.js
@@ -10,7 +10,7 @@ module.exports = sequelize.define('user', {
 		primaryKey: true,
 	},
 
-	username: {
+	email: {
 		type: Sequelize.STRING,
     allowNull: false,
     unique: true,
